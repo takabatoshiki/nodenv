a4099de278aa482627a4f44524b510506cd2d1d7
高場利樹 <takajoutoshiki@takabarijunoMacBook-Air-2.local>
2019-01-01 15:44:56 +0700

    add env

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+// @flow
+export class Hello {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+    this.say();
+  }
+
+  say(): void {
+    console.log(`Hello ${this.name} World!`);
+  }
+}
+
+export default new Hello('Neko');
