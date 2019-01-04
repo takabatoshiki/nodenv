93f955568a2f63274eb7da2b45b3109029678df3
高場利樹 <takajoutoshiki@takabarijunoMacBook-Air-2.local>
2019-01-04 16:45:51 +0700

    add prod

--- webpack.common.js
+++ webpack.common.js
@@ -1,30 +1,18 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 const src = path.join(__dirname, 'src');
-const dist = path.join(__dirname, 'dist');
+const prodMode = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  mode: 'development',
   entry: path.resolve(src, 'js/render.jsx'),
-  output: {
-    filename: 'index.bundle.js',
-    path: dist,
-  },
   resolve: {
     modules: ['node_modules'],
     extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        enforce: 'pre',
-        loader: 'eslint-loader',
-      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -48,19 +36,12 @@ module.exports = {
       },
     ],
   },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: dist,
-    hot: true,
-    port: 3000,
-  },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(src, 'html/index.html'),
     }),
     new MiniCSSExtractPlugin({
-      filename: 'app.css',
+      filename: prodMode ? 'app.min.css' : 'app.css',
     }),
   ],
 };
